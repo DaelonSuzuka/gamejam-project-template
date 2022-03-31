fb1b4488b4f45b6479a2a46bb847f3a2a37b6472
David Kincaid <daelonsuzuka@gmail.com>
2022-03-31 03:34:41 -0400

    Created topdown directory

--- godot/system/Scenes.gd
+++ godot/system/Scenes.gd
@@ -5,6 +5,6 @@ extends Node
 var registry = {
 	mainmenu = "res://menus/MainMenu.tscn",
 	devroomiso = "res://isometric/scenes/DevRoomIso.tscn",
-	devroomtop = "res://scenes/DevRoomTop.tscn",
+	devroomtop = "res://topdown/scenes/DevRoomTop.tscn",
 	devroomside = "res://sidescroller/scenes/DevRoomSide.tscn",
 }
