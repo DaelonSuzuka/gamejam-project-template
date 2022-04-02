f7c5ca0185407f57325728717d379e8843ef299e
David Kincaid <daelonsuzuka@gmail.com>
2022-04-02 16:38:17 -0400

    Updated InputManager

--- godot/system/InputManager.gd
+++ godot/system/InputManager.gd
@@ -33,11 +33,49 @@ func _notification(what) -> void:
 
 # ******************************************************************************
 
-func _physics_process(delta) -> void:
+func _input(event):
 	if paused:
 		return
+
 	for action in actions:
-		if Input.is_action_pressed(action) != state[action]:
-			state[action] = Input.is_action_pressed(action)
-			var event = FakeEvent.new(action, state[action])
-			emit_signal('input_event', event)
+		if event.is_action(action):
+			if event.pressed != state[action]:
+				state[action] = event.pressed
+				var _event = FakeEvent.new(action, state[action])
+				emit_signal('input_event', _event)
+
+# ******************************************************************************
+	
+# this is a dummy class that lets us pretend to send actions across the network
+class FakeEvent extends Node:
+	var action = ''
+	var pressed = false
+
+	func _init(name='', value=false):
+		action = name
+		pressed = value
+
+	func is_action(name):
+		return action == name
+
+	func is_action_pressed(name):
+		if action == name:
+			return pressed
+			
+	func is_action_released(name):
+		if action == name:
+			return !pressed
+
+	func to_dict():
+		return {
+			'action': action,
+			'pressed': pressed,
+		}
+
+	func from_dict(dict):
+		action = dict.action
+		pressed = dict.pressed
+		return self
+
+	func from_event(event):
+		pass
