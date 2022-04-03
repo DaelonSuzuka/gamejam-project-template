142e2a5b075e3f5815f2d0abe5917f4138acb675
David Kincaid <daelonsuzuka@gmail.com>
2022-04-03 09:00:50 -0400

    Fixed error caused by unsafe texture loading

--- godot/scenes/Eyes.gd
+++ godot/scenes/Eyes.gd
@@ -1,23 +1,31 @@
 extends Control
 
+# ******************************************************************************
+
 const MAX_CLOSED = 100
-export var LeftClosed:int = 0
-export var RightClosed:int = 0
+export var left_closed := 0
+export var right_closed := 0
 
-func _process(delta: float) -> void:
-	if LeftClosed == MAX_CLOSED: return
+onready var Left := $Left
+onready var Right := $Right
+
+# ******************************************************************************
+
+func _physics_process(delta: float) -> void:
+	if left_closed == MAX_CLOSED and right_closed == MAX_CLOSED:
+		return
 		#death
-		#LeftClosed = 0
-		#RightClosed = 0
+		#left_closed = 0
+		#right_closed = 0
 
-	LeftClosed += 1
-	RightClosed += 1
-	$Left.texture = load("res://assets/eyes"+String(1+LeftClosed/25)+".png")
-	$Right.texture = load("res://assets/eyes"+String(1+RightClosed/25)+".png")
+	left_closed += 1
+	right_closed += 1
 
+	Left.frame = int(1 + left_closed / 25)
+	Right.frame = int(1 + right_closed / 25)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("AwakeLeft"):
-		LeftClosed = max(0,LeftClosed-25)
+		left_closed = max(0, left_closed - 25)
 	if event.is_action_pressed("AwakeRight"):
-		RightClosed = max(0,RightClosed-25)
+		right_closed = max(0, right_closed - 25)
