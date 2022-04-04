d9fba2652c2d9276b658ea88d2ef80272f5b323f
David Kincaid <daelonsuzuka@gmail.com>
2022-04-04 15:23:19 -0400

    Added debug controls

--- godot/global_canvas/GlobalCanvas.gd
+++ godot/global_canvas/GlobalCanvas.gd
@@ -2,6 +2,28 @@ extends CanvasLayer
 
 # ******************************************************************************
 
+func _ready():
+	for child in $DebugControls.get_children():
+		if child is Button:
+			child.connect('pressed', self, 'debug_button', [child])
+
+func debug_button(button):
+	match button.name:
+		'Eyes':
+			$Eyes.active = !$Eyes.active
+		'Countryside':
+			Game.load_scene('countryside')
+		'City':
+			Game.load_scene('city')
+		'Beach':
+			Game.load_scene('beach')
+		'Forest':
+			Game.load_scene('forest')
+		'House':
+			Game.load_scene('house')
+
+# ******************************************************************************
+
 func show_debug():
 	$Debug.show()
 
