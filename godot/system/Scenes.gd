f05046f94c0b558356217b48451bdfc4b8e52b1a
David Kincaid <daelonsuzuka@gmail.com>
2022-04-04 08:57:02 -0400

    Removed old scenes and updated scene map

--- godot/system/Scenes.gd
+++ godot/system/Scenes.gd
@@ -5,7 +5,11 @@ extends Node
 var registry = {
 	mainmenu = "res://menus/MainMenu.tscn",
 	devroom = "res://scenes/DevRoom.tscn",
-	city = 'res://scenes/City.tscn',
-	beach = 'res://scenes/Beach.tscn',
-	countryside = 'res://scenes/Countryside.tscn',
+	city = 'res://levels/scenes/City.tscn',
+	beach = 'res://levels/scenes/Beach.tscn',
+	beach2 = 'res://levels/scenes/Beach2.tscn',
+	countryside = 'res://levels/scenes/Countryside.tscn',
+	forest = 'res://levels/scenes/Forest.tscn',
+	house = 'res://levels/scenes/House.tscn',
+	house2 = 'res://levels/scenes/House2.tscn',
 }
