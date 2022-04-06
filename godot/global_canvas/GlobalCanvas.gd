d9b6f0781cf04898f9a0b069001e5238a25b27c1
rbecker5 <robin-gilles.becker@uld.be>
2022-04-06 08:41:52 +0200

    fixed debug and cave

--- godot/global_canvas/GlobalCanvas.gd
+++ godot/global_canvas/GlobalCanvas.gd
@@ -53,7 +53,7 @@ func start_tutorial():
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("debug+"):
-		$Debug.visible = !$Debug.visible
+		$DebugControls.visible = !$DebugControls.visible
 
 
 
